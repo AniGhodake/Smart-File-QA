cb3cc3397677de7265c6c639ce0f8539a16b1ae2
Aniket_Ghodake <ghodakeaniket27@gmail.com>
2025-09-05 15:14:30 +0530

    added basic multer upload route

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express  = require('express');
+const multer =require('multer');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
+
+
+// Multer setup                                         
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limit: {fileSize:5 * 1024 * 1024},                   // ********** 5MB limit
+    fileFilter:(req,file,cb)=>{
+        const allowedTypes = ['image/jpeg','image/png','application/pdf'];
+    if(allowedTypes.includes.includes(file.minetype)){
+        cb(null,true);
+    }
+    else{
+        cb(new Error('Only JPG, PNG or PDF files are allowed'));
+    }
+    }
+});
+
+
+//Upload route 
+app.post('/upload',upload.single('file'),(req,res)=>{
+    if(!req.file){
+        return res.status(400).json({error:'No file uploaded'});
+    }
+    res.json({filename: req.file.originalname, type: req.file.minetype});
+})
+
+
 
 app.get('/',(req,res)=>{
     res.send('My App Works!!!');
